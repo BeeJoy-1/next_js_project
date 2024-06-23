06b6dc6ee59b9e0f16c2694375a7f67d3d101b8d
BeeJoy-1 <beejoy3000@gmail.com>
2024-06-23 09:27:11 +0600

    fixed some issues

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -16,7 +16,7 @@ const RecentProject = () => {
             {projects.map(({id, title, des, img, iconLists,link,}) => (
                 <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
                     <PinContainer title={link} href={link}>
-                        <div className=' relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] sm:h-[40vh] mb-10'>
+                        <div className=' relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[250px] sm:h-[350px] mb-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                                 <img src='/bg.png' alt='bg-img'/>
                             </div>
